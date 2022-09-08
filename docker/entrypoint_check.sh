e0ca387fcf5362d41c9d10093c7f53d724f0f05b
ElsLommelen <els.lommelen@inbo.be>
2022-09-08 11:51:03 +0200

    delete superfluous code (due to update of protocolhelper)

--- docker/entrypoint_check.sh
+++ docker/entrypoint_check.sh
@@ -16,17 +16,12 @@ UPDATED=$(Rscript -e 'protocolhelper::update_version_number("'$PROTOCOL_CODE'")'
 echo 'output updated:' $UPDATED
 if [ "$UPDATED" = "[1] TRUE" ]; then
   # remake the last commit without deletion of .Rprofile
-  MESSAGE=$(git log -1 --pretty=%B)
-  git reset --soft HEAD^
-  git reset HEAD .Rprofile
-  git commit --message="$MESSAGE"
 
   git push -f
   echo '\ncommit with new version pushed\n'
 fi
 
 echo '\nChecking protocols specific tests...\n'
-rm .Rprofile  #again, as it is added again while making the commit
 
 Rscript "docker/check_all.R"
 if [ $? -ne 0 ]; then
