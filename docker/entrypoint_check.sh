46e172b8daac5fe542844def3e6a39b389bcc172
ElsLommelen <els.lommelen@inbo.be>
2022-11-23 14:34:28 +0100

    Add error message as discussed in https://github.com/ElsLommelen/testprotocol/pull/38#issuecomment-1324977218

--- docker/entrypoint_check.sh
+++ docker/entrypoint_check.sh
@@ -17,7 +17,7 @@ echo 'FOLDERNAME_REL=' $FOLDERNAME_REL
 CHANGED=$(git diff --name-only main | grep -v NEWS\.md | grep -v \.zenodo\.json | grep -v \.Rprofile | grep -v ^$FOLDERNAME_REL/)
 
 if [ -n "${CHANGED}" ]; then
-  echo '\nBranch '$GITHUB_HEAD_REF' contains changes to files which do not belong to protocol '$GITHUB_HEAD_REF', and these changes will not be passed to the website. Please remove these changes from this branch and start a new branch for changes to other protocols. Changes to be removed:' $CHANGED
+  echo '\nBranch '$GITHUB_HEAD_REF' contains changes to files which do not belong to protocol '$GITHUB_HEAD_REF', and these changes will not be passed to the website. Please remove these changes from this branch and start a new branch for changes to other protocols. Maybe a wrong protocol_code is used as branch name? Changes to be removed:' $CHANGED
   exit 1
 fi
 
