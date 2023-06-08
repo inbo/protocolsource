41bfc00cab4ea826ca4cfabe5f510c9777dad7b5
OOSTERLYNCK <patrik.oosterlynck@inbo.be>
2023-06-08 15:27:52 +0200

    edits hfdstk benodigdheden en werkwijze

--- source/spp/mhq/spp_001_nl_mhq_heide/setup_mhq_heideprotocol.R
+++ source/spp/mhq/spp_001_nl_mhq_heide/setup_mhq_heideprotocol.R
@@ -1,19 +1,20 @@
 library(protocolhelper)
-library(dplyr)
-install.packages("readxl")
-library(readr)
-library(readxl)
-# create_protocol(
-#   protocol_type = "spp",
-#   title = "Veldprotocol Kwaliteitsmeetnet Natura 2000 habitats partim heidehabitats",
-#   short_title = "mhq heide",
-#   authors = c("Oosterlynck, Patrik"),
-#   orcids = c("0000-0002-5712-0770"),
-#   reviewers = c("Hans Van Calster", "Toon Westra", "Leen Govaere"),
-#   file_manager = "Patrik Oosterlynck",
-#   project_name = "mhq",
-#   language = "nl",
-#   subtitle = "psammofiele heide (2310), buntgrasvegetaties (2330_bu), droge heide (4030), natte heide (4010)")
+
+create_protocol(
+                protocol_type = "spp",
+                title = "Veldprotocol Kwaliteitsmeetnet Natura 2000 habitats partim heidehabitats",
+                short_title = "mhq heide",
+                authors = c("Oosterlynck, Patrik"),
+                orcids = c("0000-0002-5712-0770"),
+                reviewers = c("Hans Van Calster", "Toon Westra", "Leen Govaere"),
+                file_manager = "Patrik Oosterlynck",
+                project_name = "mhq",
+                language = "nl",
+                subtitle = "psammofiele heide (2310), 
+                            buntgrasvegetaties (2330_bu), 
+                            droge heide (4030), 
+                            natte heide (4010)"
+                )
 
 render_protocol(protocol_code = "spp-001-nl")
 #renv::restore()
